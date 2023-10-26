6ffea68d900054e52d988a933a43970e7e06eff0
ELVIS1230 <61919996+ELVIS1230@users.noreply.github.com>
2023-10-25 23:30:38 -0500

    Creacion del template

--- template-angular/src/app/app.component.spec.ts
+++ template-angular/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'template-angular'`, () => {
+  it(`should have as title 'angular-ejer1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('template-angular');
+    expect(app.title).toEqual('angular-ejer1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('template-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ejer1 app is running!');
   });
 });
---
+++ template-angular/src/app/web/web.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    CategoryComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WebModule { }
